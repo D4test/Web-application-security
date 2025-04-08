dc2fe63453acbc20c4f0215f15cd34fa6ab93e01
D4test <125992802+D4test@users.noreply.github.com>
2025-04-08 14:43:48 +0300

    Add files via upload

---
+++ lab6/lab6/src/main/java/edu/kiligoy/lab6/config/AuditionConfig.java
@@ -0,0 +1,25 @@
+package edu.kiligoy.lab6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+/*
+@author   Dtest
+@project   lab5
+@class  AuditionConfig
+@version  1.0.0
+@since 08.04.2025 - 13.12
+*/
+
+@EnableMongoAuditing
+@Configuration
+public class AuditionConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAwareImpl();
+    }
+
+}
---
+++ lab6/lab6/src/main/java/edu/kiligoy/lab6/item/Item.java
@@ -0,0 +1,44 @@
+package edu.kiligoy.lab6.item;
+
+/*
+@author   Dtest
+@project   lab5
+@class  Item
+@version  1.0.0
+@since 08.04.2025 - 13.11
+*/
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Item extends AuditMetaData {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Item item)) return false;
+
+        return getId().equals(item.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+
+}
